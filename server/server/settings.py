bcd1cf7e24cdfa3b2fc3ef2052b5723c1cc7e022
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-07-12 08:38:37 +0500

    feat(server): /courses added

--- server/server/settings.py
+++ server/server/settings.py
@@ -53,7 +53,9 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 
 ROOT_URLCONF = 'server.urls'
 
