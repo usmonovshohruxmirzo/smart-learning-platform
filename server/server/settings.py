f44040c8df92d026d0715aaf7c7aaf6e6c254efe
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-07-12 16:50:05 +0500

    feat(courses): initialize courses app with models, serializers, views, and admin registration; set up URLs for course API

--- server/server/settings.py
+++ server/server/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'courses',
+
+    'apps.courses',
+    'apps.students',
 ]
 
 MIDDLEWARE = [
--- server/server/urls.py
+++ server/server/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('courses.urls')),  # <-- add this
+    path('api/', include('apps.courses.urls')),
 ]
