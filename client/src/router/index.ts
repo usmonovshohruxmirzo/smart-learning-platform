8d112a2d93fe206ec5cee763e4c049629ff64ac0
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-07-13 17:05:47 +0500

    refactor(auth): Login & Register Views moved to /auth folder

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -11,12 +11,12 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/LoginView.vue'),
+      component: () => import('../views/auth/LoginView.vue'),
     },
     {
       path: '/register',
       name: 'register',
-      component: () => import('../views/RegisterView.vue'),
+      component: () => import('../views/auth/RegisterView.vue'),
     },
     {
       path: '/dashboard',
