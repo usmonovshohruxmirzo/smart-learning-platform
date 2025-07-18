f7ef453f92b3558d5bf0eefd740b68d0543ed3ad
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-07-18 13:57:50 +0500

    init(server): added /enroll views

--- server/apps/enroll/views.py
+++ server/apps/enroll/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from courses.models import Course
+from .models import Enrollment, LessonProgress
+from .serializers import EnrollmentSerializer, LessonProgressSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+class EnrollView():
+    pass
+
+class MarkLessonCompleteView():
+    pass
+
+class CourseProgressView():
+    pass
